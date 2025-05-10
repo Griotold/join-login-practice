0a6b9289b6a4dc76a03bb19fea6fa7581c180939
Griotold <gotjd9773@naver.com>
2025-05-10 12:09:21 +0900

    Initial commit

---
+++ src/main/java/com/griotold/join_login/JoinLoginApplication.java
@@ -0,0 +1,13 @@
+package com.griotold.join_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/griotold/join_login/JoinLoginApplicationTests.java
@@ -0,0 +1,15 @@
+package com.griotold.join_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class JoinLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
