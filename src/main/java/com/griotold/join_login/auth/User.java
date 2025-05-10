7d9ea7997c4dca5d96628ae1740e14ef0f890144
Griotold <gotjd9773@naver.com>
2025-05-10 12:29:31 +0900

    User 엔티티, UserRepository

---
+++ src/main/java/com/griotold/join_login/auth/User.java
@@ -0,0 +1,33 @@
+package com.griotold.join_login.auth;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "a_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email; // username
+
+    private String password;
+
+    public static User create(String name, String email, String password) {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .build();
+    }
+}
